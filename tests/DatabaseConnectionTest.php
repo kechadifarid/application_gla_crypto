eb10aa36df6e7e1c0e696469a9d0ef4ee326c9a8
kecha <kechadifarid10@gmail.com>
2024-12-29 11:06:35 +0100

    ajour workflow

--- tests/DatabaseConnectionTest.php
+++ tests/DatabaseConnectionTest.php
@@ -4,7 +4,7 @@ namespace Test;
 use PHPUnit\Framework\TestCase;
 use App\DatabaseConnection;  // Assurez-vous que ce 'use' est correct
 
-class ConnectionTest extends TestCase {
+class DatabaseConnectionTest extends TestCase {
     private $db;
 
     protected function setUp(): void {
